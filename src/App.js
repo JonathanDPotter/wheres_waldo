2ea28b7fc33b76971dc31a25485322755b3cbfb7
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-08-10 14:50:45 -0400

    reset

--- src/App.js
+++ src/App.js
@@ -5,26 +5,48 @@ import Picture from "./components/Picture/Picture";
 import ChooseScreen from "./components/ChooseScreen/ChooseScreen";
 import WinScreen from "./components/WinScreen/WinScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+  highScores: [],
+  currentTime: 0,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "countdown":
+      return { currentTime: state.currentTime - 1 };
+    default:
+      return state;
+  }
+}
+
+let store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <ChooseScreen />
-          </Route>
-          <Route path="/game">
-            <Picture />
-            <Sidebar />
-          </Route>
-          <Route path="/winscreen">
-            <WinScreen />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <ChooseScreen />
+            </Route>
+            <Route path="/game">
+              <Picture />
+              <Sidebar />
+            </Route>
+            <Route path="/winscreen">
+              <WinScreen />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
